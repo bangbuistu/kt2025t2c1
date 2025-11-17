e9fdc6b790668209a24411cc0493a1dbd25b133c
nikanika22 <dinhhuyteam@gmail.com>
2025-11-17 08:29:59 +0700

    kiem tra am duong

---
+++ kt.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Nhap vao mot so ";
+    int n;
+    cin>>n;
+    if(n>0)
+    cout<<"la so duong"
+    else 
+    cout<<"la so am";
+    return 0;
+}
