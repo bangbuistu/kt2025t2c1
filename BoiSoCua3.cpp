837332fbbb88bc6191066749d1e9ca4e5825efec
phuongkhuu <thanhphuongkhuu@gmail.com>
2025-11-17 08:31:53 +0700

    Hướng dẫn dùng cpp

---
+++ BoiSoCua3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cout << "Nhap vao mot so nguyen: ";
+	cin >> a;
+	if (a % 3 == 0) {
+		cout << a << " la boi so cua 3.";
+	}
+	else
+		cout << a << " khong phai boi so cua 3.";
+	return 0;
+}
