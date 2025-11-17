d66f986adf710b1b25991fb8a37bce5e8c46d33c
TranKhuongDuy-sabo <duyt2296@gmail.com>
2025-11-17 08:48:24 +0700

    update

--- cau2_trankhuongduy.cpp
+++ cau2_trankhuongduy.cpp
@@ -3,15 +3,15 @@
 void main(){
  void kiemtrasongto(a){
 	if(a <= 2)
-	cout<< " day khong phai so nguyento"
+	cout<< " day khong phai so nguyen"
 	if (a=2)
-	return " Day la so nguyen to chan "
+	return " Day la so nguyen chan "
  	for ( int i=2; i< a;i++){
 	if(a%i==0)
-		cout<<" day khong phai la so nguyen to"
+		cout<<" day khong phai la so nguyen"
  	}
 	if(a%2==0)
-		cout<<"day la so ngto chan "
+		cout<<"day la so chan "
 	else
-		cout<<"day la so ngto le"
+		cout<<"day la so le"
 }
